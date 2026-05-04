82e5acf6568c8f825c6833b12bb95fcd624eea41
MaheshGudooru <mahesh.g@techouts.co.in>
2026-05-04 16:26:15 +0530

    delete: remove testing message from Demo class

--- Demo.java
+++ Demo.java
@@ -2,7 +2,6 @@ class Demo {
 
     public static void main (String[] args) {
 
-        System.out.println("This is a testing commit!!");
         System.out.println("Demo project is running successfully.........");
 
     }
