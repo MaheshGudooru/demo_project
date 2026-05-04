4f6701389098c082608b826ec962b5c8cd6bdab0
MaheshGudooru <mahesh.g@techouts.co.in>
2026-05-04 15:58:32 +0530

    add testing message to Demo class

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ class Demo {
 
     public static void main (String[] args) {
 
+        System.out.println("This is a testing commit!!");
         System.out.println("Demo project is running successfully.........");
 
     }
